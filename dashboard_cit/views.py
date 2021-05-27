b7d3531739e5a942a67b8c33bbbd9a4bbd5ad6c4
GanapathyPT <18ganapathy04@gmail.com>
2021-05-27 13:14:43 +0530

    added validation for registration

--- dashboard_cit/views.py
+++ dashboard_cit/views.py
@@ -13,6 +13,17 @@ class SignupView(View):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
+        user_with_username = User.objects.filter(username=username)
+        if user_with_username.exists():
+            return render(request, "registration/signup.html", { "error": "Username Already Taken" })
+
+        user_with_email = User.objects.filter(email=email)
+        if user_with_email.exists():
+            return render(request, "registration/signup.html", { "error": "Email Already Exists" })
+
+        if len(password) < 5:
+            return render(request, "registration/signup.html", { "error": "Password is too weak" })
+
         user = User.objects.create_user(username, email, password)
         user.save()
 
