e730c6b1e3874f09f613515ee2f256779091f3f5
GanapathyPT <18ganapathy04@gmail.com>
2021-06-13 16:37:36 +0530

    added excel download feature

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dashboard.views import BiddingView, DashboardView
+from dashboard.views import *
 
 urlpatterns = [
     path("", DashboardView, name="dashboard"),
     path("bidding/", BiddingView.as_view(), name="bidding"),
+    path("bidding/<int:pk>/download/", DownloadExcellView),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
 from dashboard.models import Bidding, BiddingRow
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.decorators import login_required
 import datetime
+import xlsxwriter
 
 
 @login_required
@@ -64,8 +65,54 @@ class BiddingView(LoginRequiredMixin, View):
         bidding.total_qty = total_qty
         bidding.save()
 
+        if "excel" in request.POST:
+            # create a excel file and send to user
+            return redirect(f"/bidding/{bidding.id}/download/")
+
         return render(
             request,
             "bidding/index.html",
             {"dummy_arr": list(range(10)), "message": "Bidding Saved"},
         )
+
+
+@login_required
+def DownloadExcellView(request, pk):
+    user = request.user
+    bidding = Bidding.objects.get(pk=pk)
+
+    workbook = xlsxwriter.Workbook(f"media/files/{user.username}-Bidding.xlsx")
+    worksheet = workbook.add_worksheet()
+    heading_format = workbook.add_format({"bold": True})
+
+    worksheet.write("A1", "Slot", heading_format)
+    worksheet.write("B1", "From", heading_format)
+    worksheet.write("C1", "To", heading_format)
+    worksheet.write("D1", "Time Block", heading_format)
+    worksheet.write("E1", "Buy Quantity", heading_format)
+    worksheet.write("F1", "Price", heading_format)
+
+    rows = BiddingRow.objects.filter(bidding=bidding)
+    row_index = 2
+
+    for row in rows:
+        worksheet.write(f"A{row_index}", row.slot)
+        worksheet.write(f"B{row_index}", row.from_time)
+        worksheet.write(f"C{row_index}", row.to_time)
+        worksheet.write(f"D{row_index}", row.time_block)
+        worksheet.write(f"E{row_index}", row.quantity)
+        worksheet.write(f"F{row_index}", row.price)
+
+        row_index += 1
+
+    worksheet.write(f"D{row_index}", "TOTAL")
+    worksheet.write(f"E{row_index}", f"=SUM(E2:E{row_index - 1})")
+    worksheet.write(f"F{row_index}", f"=SUM(F2:F{row_index - 1})")
+
+    workbook.close()
+
+    response = HttpResponse(workbook)
+    response[
+        "Content-Disposition"
+    ] = f"attachment; filename={user.username}-Bidding.xlsx"
+    return response
